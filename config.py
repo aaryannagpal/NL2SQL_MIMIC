b9f54af8e6ce82f63d24117c6fe5661610ab866d
Aaryan Nagpal <aaryannagpal@gmail.com>
2025-04-03 02:42:55 +0530

    Shifted from MySQL to SQLite3

--- config.py
+++ config.py
@@ -1,12 +1,6 @@
 import os
 
-# Database Config
-DB_CONFIG = {
-    "host": "localhost",
-    "user": "aaryan",
-    "password": os.getenv("MIMIC_SQL_PW"),
-    "database": "capstone_mimic"
-}
+MYSQL_DB_PATH = "/home/aaryan/Downloads/mimic/mimic4.db"
 
 # OpenAI API Key (GPT-4)
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
