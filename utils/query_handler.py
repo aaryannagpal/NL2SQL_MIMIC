68710255cef7bf781652328dd7e27af9e562afd6
Aaryan Nagpal <aaryannagpal@gmail.com>
2025-04-17 19:31:27 +0530

    Added logic to execute dataset queries

--- utils/query_handler.py
+++ utils/query_handler.py
@@ -9,6 +9,20 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from config import MYSQL_DB_PATH
 import pandas as pd
 
+CURRENT_DATE = "2100-12-31"
+CURRENT_TIME = "23:59:00"
+NOW = f"{CURRENT_DATE} {CURRENT_TIME}"
+PRECOMPUTED_DICT = {
+    'temperature': (35.5, 38.1),
+    'sao2': (95.0, 100.0),
+    'heart rate': (60.0, 100.0),
+    'respiration': (12.0, 18.0),
+    'systolic bp': (90.0, 120.0),
+    'diastolic bp': (60.0, 90.0),
+    'mean bp': (60.0, 110.0)
+}
+TIME_PATTERN = r"(DATE_SUB|DATE_ADD)\((\w+\(\)|'[^']+')[, ]+ INTERVAL (\d+) (MONTH|YEAR|DAY)\)"
+
 class QueryHandler:
 
     def __init__(self):
@@ -44,10 +58,10 @@ class QueryHandler:
         - error message (if any)
         - query plan (if supported)
         """
-        # cleaned_query = self.query_cleanup(sql_query)
+        cleaned_query = self.query_cleanup(sql_query)
         response = {
             "query": sql_query,
-            # "cleaned_query": cleaned_query,
+            "cleaned_query": cleaned_query,
             "success": False,
             "execution_time": 0,
             "row_count": 0,
@@ -91,6 +105,7 @@ class QueryHandler:
         """
         Execute SQL query and return as DataFrame.
         """
+        query = self.query_cleanup(sql_query)
         try:
             df = pd.read_sql_query(query, self.connection)
             if 'index' in df.columns and not keep_index:
@@ -102,6 +117,57 @@ class QueryHandler:
         except Exception as e:
             print(f"Error: {e}")
             return None
+
+    def convert_date_function(self, match):
+        function = match.group(1)
+        date = match.group(2)
+        number = match.group(3)
+        unit = match.group(4).lower()
+        
+        # Use singular form when number is 1
+        if number == '1':
+            unit = unit.rstrip('s')
+        else:
+            unit += 's' if not unit.endswith('s') else ''
+        
+        # Determine the sign based on the function (DATE_SUB or DATE_ADD)
+        sign = '-' if function == 'DATE_SUB' else '+'
+        
+        return f"datetime({date}, '{sign}{number} {unit}')"
+
+    def query_cleanup(self, query):
+ 
+        query = re.sub('[ ]+', ' ', query.replace('\n', ' ')).strip()
+        query = query.replace('> =', '>=').replace('< =', '<=').replace('! =', '!=')
+
+        query = re.sub(TIME_PATTERN, self.convert_date_function, query)
+
+        if "current_time" in query: # strftime('%J',current_time) => strftime('%J','2100-12-31 23:59:00')
+            query = query.replace("current_time", f"'{NOW}'")
+        if "current_date" in query: # strftime('%J',current_date) => strftime('%J','2100-12-31')
+            query = query.replace("current_date", f"'{CURRENT_DATE}'")
+        if "'now'" in query: # 'now' => '2100-12-31 23:59:00'
+            query = query.replace("'now'", f"'{NOW}'")
+        if "NOW()" in query: # NOW() => '2100-12-31 23:59:00'
+            query = query.replace("NOW()", f"'{NOW}'")
+        if "CURDATE()" in query: # CURDATE() => '2100-12-31'
+            query = query.replace("CURDATE()", f"'{CURRENT_DATE}'")
+        if "CURTIME()" in query: # CURTIME() => '23:59:00'
+            query = query.replace("CURTIME()", f"'{CURRENT_TIME}'")
+            
+        if re.search('[ \n]+([a-zA-Z0-9_]+_lower)', query) and re.search('[ \n]+([a-zA-Z0-9_]+_upper)', query):
+            vital_lower_expr = re.findall('[ \n]+([a-zA-Z0-9_]+_lower)', query)[0]
+            vital_upper_expr = re.findall('[ \n]+([a-zA-Z0-9_]+_upper)', query)[0]
+            vital_name_list = list(set(re.findall('([a-zA-Z0-9_]+)_lower', vital_lower_expr) + re.findall('([a-zA-Z0-9_]+)_upper', vital_upper_expr)))
+            if len(vital_name_list)==1:
+                processed_vital_name = vital_name_list[0].replace('_', ' ')
+                if processed_vital_name in PRECOMPUTED_DICT:
+                    vital_range = PRECOMPUTED_DICT[processed_vital_name]
+                    query = query.replace(vital_lower_expr, f"{vital_range[0]}").replace(vital_upper_expr, f"{vital_range[1]}")
+
+        query = query.replace("%y", "%Y").replace('%j', '%J')
+
+        return query
 
     def __del__(self):
         if hasattr(self, 'connection') and self.connection:
