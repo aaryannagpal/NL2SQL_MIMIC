dd91cedfc8c6e7156891e671ba9aee26ae04238f
aaryannagpal <aaryannagpal@gmail.com>
2025-05-08 23:45:28 +0530

    Final FT Eval file

--- model/m2/fine_eval.py
+++ model/m2/fine_eval.py
@@ -3,14 +3,14 @@ import sys
 PROJECT_ROOT = Path().resolve().parent.parent
 sys.path.append(str(PROJECT_ROOT))
 
-from utils.model_utils import M1Tester
+from utils.model_utils import M2Tester
 
-model = 'duckdb_nsql'
+model = "qwen_2.5"
 
-m2_tester = M2Tester('duckdb_nsql')
+m2_tester = M2Tester(model)
 
-m2, m2_tokenizer = m2_tester.load_model()
+_, _ = m2_tester.load_model()
 
-eval_df = m2_tester.evaluate_model()
+eval_df = m2_tester.evaluate_model('Qwen_2.5_14b_results')
 eval_df.to_csv(f"{model}.csv", index=False)
 print(f"Saved model with {len(eval_df)} rows")
